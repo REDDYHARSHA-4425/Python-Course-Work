dd7f20f84779ba16516ae442b253f1bf0b5053e9
REDDY HARSHA <reddyvarsha4646@gmail.com>
2026-07-22 10:57:58 +0530

    Add files via upload

---
+++ Day-2/variable operations.py
@@ -0,0 +1,27 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=10
+>>> A=20
+>>> a
+10
+>>> A
+20
+>>> a=10
+>>> a=b=c=10
+>>> a,b,c=10,20,30
+>>> a
+10
+>>> b
+20
+>>> c
+30
+>>> a
+10
+>>> b
+20
+>>> a,b=b,a
+>>> a
+20
+>>> b
+10
+>>> del a
