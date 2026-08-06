b5a73434d732c3358e228cf91d28ce173ad6400d
REDDY HARSHA <reddyvarsha4646@gmail.com>
2026-08-06 10:58:09 +0530

    Add files via upload

---
+++ Day-11/elifladder.py
@@ -0,0 +1,33 @@
+'''budget=int(input())
+if budget>10000:
+    print("Trip")
+elif budget>5000:
+    print("Resort Stay")
+elif budget>3000:
+    print("movie and dinner")
+elif budget >1000:
+    print("cafe and shopping")
+elif budget>500:
+    print("Street food and park visit")
+else:
+    print("Stay home")
+       '''
+'''time=int(input())
+if 5<=time<=11:
+    print("Good Morning")
+elif 12<=time<=16:
+    print("Good Afternoon")
+elif 17<=time<=20:
+    print("Good evening")
+else:
+    print("Good night")    
+    '''
+budget=int(input())
+if budget>10000:
+    print("cloud hosting")
+elif budget>5000:
+    print("business hosting")
+elif budget >3000:
+    print("premium hosting")
+else:
+    print("single hosting")
---
+++ Day-11/ifelse.py
@@ -0,0 +1,19 @@
+'''username=input("username: ")
+password=input("Password: ")
+if username == 'admin' and password=='admin123':
+    print("Login Successfull")
+else:
+    print("Invalid Credentials")
+    '''
+'''products=['Laptop','watch','Mobile']
+search=input("search product: ")
+if search in products:
+    print("Product found")
+else:
+    print("Not found")
+    '''
+bill=int(input())
+if bill>99:
+    print(bill)
+else:
+    print(bill+30)
