295eaa3e07eb8f5874cf1c6e715834672ff88384
REDDY HARSHA <reddyvarsha4646@gmail.com>
2026-08-06 11:00:30 +0530

    Add files via upload

---
+++ Day-15/assert_pass.py
@@ -0,0 +1,15 @@
+'''if 10%2==0:
+    pass'''
+'''for i in range(1,10):
+    pass
+class classname:
+    pass
+def fucnt():
+    pass'''
+#assert keyword
+'''a=10
+b=0
+assert b>0,"b is not equqal zero"
+print(a/b)
+'''
+
---
+++ Day-15/patterns.py
@@ -0,0 +1,59 @@
+'''for i in range(5):
+    for j in range(3):
+        print("*",end=" ")
+    print()'''# for new line 
+'''for i in range(7):
+    for j in range(4):
+        print("*",end=" ")
+    print()
+    '''
+'''
+for row in range(5):
+    for col in range(5):
+        print("*",end=" ")
+    print()   
+    '''
+'''
+for i in range(5):
+    for j in range(5):
+        print(i,end=" ")
+    print()
+    '''
+'''for i in range(5):
+    for j in range(5):
+        print(j,end=" ")
+    print()
+    '''
+'''for i in range(5):
+    for j in range(5):
+        print(i+j,end=" ")
+    print()
+    '''
+'''for i in range(5):
+    for j in range(5):
+        print(j%2,end=" ")
+    print()
+    '''
+'''for i in range(5):
+    for j in range(5):
+        print((i+j)%2,end=" ")
+    print()'''
+'''
+for i in range(5):
+    for j in range(i+1):
+        print(i,end=" ")
+    print()
+    '''
+'''
+for i in range(5):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+    '''
+'''
+n=int(input())
+for i in range(n):
+    for j in range(n-i):
+        print("*",end=" ")
+    print()
+    '''
